96104e9d3de604a7b9c2c53df9375f1568442de1
nyabangWech <nyabangwech@gmail.com>
2024-03-25 10:05:57 +0300

    added files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,39 @@
+fun main() {
+    statement("Barnie bakes brown bagels and buns")
+
+
+
+    word(20,30,40)
+    volume(3.141524)
+    pal("wow","kayak","madam")
+}
+fun statement(w:String){
+
+    println()
+
+
+
+
+
+
+}
+
+fun  word(x:Int, y:Int , w:Int){
+    var addition=  arrayOf(20, 30,40)
+    var additional=addition[0]+addition[1]+addition[2]
+    var total=(avearage(arrayOf(addition))
+    return
+
+
+
+}fun volume(radius:Double){
+    var volumes=(4/3*3.14159*3)
+    println(volumes)
+
+} fun pal(a:String,b:String,c:String) {
+    var new = "wow"
+    if (new == isPalindrome)
+
+
+
+}
